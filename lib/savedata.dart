77485b70d03964553d2ebe4dfea055dcc1f7a676
dragonDScript <agomila96@gmail.com>
2023-01-10 23:47:19 +0100

    Add savedata types

---
+++ lib/savedata.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+import 'dart:html' as html;
+import 'dart:io';
+import 'package:financial_management/finance.dart';
+
+abstract class ConfigModel {
+  List<FinancialMove> financialMoves;
+  List<DebtMove> debtMove;
+  List<BorrowMove> borrowMoves;
+  ConfigModel(
+      {required this.financialMoves,
+      required this.debtMove,
+      required this.borrowMoves});
+}
+
+abstract class SaveData {
+  Future<ConfigModel> read();
+  void write(ConfigModel saveData);
+}
+
+class FileSaveData extends SaveData {
+  final String saveDataPath = "";
+
+  @override
+  Future<ConfigModel> read() async {
+    File file = File(saveDataPath);
+    return jsonDecode(await file.readAsString());
+  }
+
+  @override
+  void write(ConfigModel saveData) {
+    File(saveDataPath).writeAsString(jsonEncode(saveData));
+  }
+
+  FileSaveData();
+}
+
+class WebStorageSaveData extends SaveData {
+  @override
+  Future<ConfigModel> read() => jsonDecode(html.window.localStorage['config']!);
+  @override
+  void write(ConfigModel saveData) {
+    html.window.localStorage['config'] = jsonEncode(saveData);
+  }
+}
+/*
+class GoogleDriveSaveData extends SaveData {
+  @override
+  Future<ConfigModel> read() {};
+  @override
+  void write(ConfigModel saveData) {};
+}
+*/
