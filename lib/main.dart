81b5e3e777acede195920ac4c5c178b0700836a8
dragonDScript <agomila96@gmail.com>
2023-01-21 14:18:41 +0100

    Remove permissions bs

--- lib/main.dart
+++ lib/main.dart
@@ -3,54 +3,12 @@ import 'package:money_manager/routes/borrow.dart';
 import 'package:money_manager/routes/debt.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:money_manager/gui.dart';
 import 'package:money_manager/finance.dart';
 import 'package:money_manager/savedata.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class PermissionRequestWidget extends StatelessWidget {
-  final Widget child;
-  const PermissionRequestWidget({super.key, required this.child});
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<PermissionStatus>(
-        future: Permission.manageExternalStorage.request(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            if (snapshot.data == PermissionStatus.granted) {
-              return child;
-            }
-            if (snapshot.data == PermissionStatus.permanentlyDenied ||
-                snapshot.data == PermissionStatus.denied) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const Text("Permission to files has been denied.",
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                  const Text(
-                      """I think it's fair, this permission is too invasive and I'm going to change that.
-                      The application is open source and never connects to the internet, but it's up to you.
-                      I wouldn't pick 'Agree' myself if the app was not mine."""),
-                  TextButton(
-                      onPressed: () => openAppSettings(),
-                      child: const Text("Open settings"))
-                ],
-              );
-            }
-          }
-          if (snapshot.hasError) {
-            return Center(child: Text(snapshot.error.toString()));
-          }
-          return const Center(child: CircularProgressIndicator());
-        });
-  }
 }
 
 class MyApp extends StatelessWidget {
